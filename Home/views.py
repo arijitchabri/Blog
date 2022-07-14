0c16441bd6b0ce272bf8239f320e643cc146ddeb
Arijit Chabri <arijitchabri2001@gmail.com>
2022-07-14 10:54:41 +0530

    Showing the blogs in the landing page

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    blogs = Blog.objects.all().reverse()
+    content = {'blogs' : blogs}
+    return render(request, 'index.html', content)
