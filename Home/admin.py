4b6edc1b19f9b44c30c98f7b647ab9d374f6f722
Arijit Chabri <arijitchabri2001@gmail.com>
2022-07-14 10:04:26 +0530

    Home and Blog models created

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tags, Blog
 
 # Register your models here.
+admin.site.register(Tags)
+admin.site.register(Blog)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return f'{self.id}  {self.tag}'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    tags = models.ManyToManyField('Tags')
+    description = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
